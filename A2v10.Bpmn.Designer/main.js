a996bfea733161325e624ec4cca45f38b930a9e0
Аlex Kukhtin <alex.kukhtin@outlook.com>
2021-08-27 17:22:20 +0300

    bpmn.designer

---
+++ A2v10.Bpmn.Designer/main.js
@@ -0,0 +1,41 @@
+
+'use strict';
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu, dialog, MenuItem } = require('electron')
+const fs = require('fs');
+const path = require('path');
+
+function createWindow() {
+	const mainWindow = new BrowserWindow({
+		width: 1024,
+		height: 768,
+		webPreferences: {
+			nodeIntegration: true,
+			contextIsolation: false,
+			enableRemoteModule: true//,
+			//preload: path.join(__dirname, 'preload.js')
+		}
+	});
+
+	// and load the index.html of the app.
+	mainWindow.loadFile('index.html')
+
+	// Open the DevTools.
+	//mainWindow.webContents.openDevTools()
+}
+
+app.whenReady().then(() => {
+	createWindow();
+
+	app.on('activate', function () {
+		if (BrowserWindow.getAllWindows().length === 0)
+			createWindow()
+	})
+});
+
+app.on('window-all-closed', function () {
+	if (process.platform !== 'darwin')
+		app.quit()
+});
+
