c6eceb949347ad25c5732a4ea29c4a779e7784af
alex-kukhtin <alex.kukhtin@outlook.com>
2025-05-20 13:24:02 +0300

    v 8224

--- A2v10.Workflow/NativeObjects/ScriptNativeDeferred.cs
+++ A2v10.Workflow/NativeObjects/ScriptNativeDeferred.cs
@@ -1,31 +1,28 @@
-﻿// Copyright © 2020-2021 Oleksandr Kukhtin. All rights reserved.
+﻿// Copyright © 2020-2025 Oleksandr Kukhtin. All rights reserved.
 
-using Microsoft.Extensions.DependencyInjection;
-using System;
 using System.Dynamic;
 
-namespace A2v10.Workflow
-{
-    public class ScriptNativeDeferred : IInjectable
-    {
-        private IDeferredTarget? _deferredTarget;
+namespace A2v10.Workflow;
 
-        public void Inject(IServiceProvider serviceProvider)
-        {
-        }
+public class ScriptNativeDeferred : IInjectable
+{
+    private IDeferredTarget? _deferredTarget;
+
+    public void Inject(IServiceProvider serviceProvider)
+    {
+    }
 
 		public void SetDeferred(IDeferredTarget deferredTarget)
-        {
+    {
 			_deferredTarget = deferredTarget;
 		}
 
 #pragma warning disable IDE1006 // Naming Styles
 		public void executeSql(String procedure, ExpandoObject? prms = null)
 #pragma warning restore IDE1006 // Naming Styles
-        {
-            if (_deferredTarget == null)
-                throw new InvalidProgramException("DeferredTarget is null");
-            _deferredTarget.AddDeffered(new DeferredElement(DeferredElementType.Sql, procedure, prms, _deferredTarget.Refer));
-        }
+    {
+        if (_deferredTarget == null)
+            throw new InvalidProgramException("DeferredTarget is null");
+        _deferredTarget.AddDeffered(new DeferredElement(DeferredElementType.Sql, procedure, prms, _deferredTarget.Refer));
     }
 }
