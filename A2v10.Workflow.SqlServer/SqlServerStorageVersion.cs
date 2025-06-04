685ddcc05312b765f311e026f014e8577ff44281
alex-kukhtin <alex.kukhtin@outlook.com>
2025-06-04 06:29:11 +0300

    v 8229

--- A2v10.Workflow.SqlServer/SqlServerStorageVersion.cs
+++ A2v10.Workflow.SqlServer/SqlServerStorageVersion.cs
@@ -12,7 +12,7 @@ internal record VersionInfo
 
 public class SqlServerStorageVersion(IDbContext dbContext) : IWorkflowStorageVersion
 {
-	private const Int32 REQUIRED_VERSION = 8227;
+	private const Int32 REQUIRED_VERSION = 8229;
 
 	private readonly IDbContext _dbContext = dbContext;
 
